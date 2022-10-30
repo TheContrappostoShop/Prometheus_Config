a1e5076ca60c7e6254d88877f6386656c77cae9e
Ada Phillips <ragwafire99@gmail.com>
2022-10-30 13:18:56 -0400

    Prometheus-Config First Commit
    Add klipper_config files, nanodlp_db files, nanodlp.service, and
    kiauh klipper_repost.txt
    Add prometheus_install.sh script, in its initial version

---
+++ prometheus_install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# add-copyright-here
+
+set -e
+
+SOURCE_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+mkdir -p ${HOME}/printer_data/config
+mkdir -p ${HOME}/nanodlp
+
+# TODO: Modify printer.cfg mcu location to match connected serial device
+cp -r ${SOURCE_DIR}/klipper_config/* ${HOME}/printer_data/config
+
+wget https://www.nanodlp.com/download/nanodlp.linux.arm64.stable.tar.gz -O - | tar -xz -C ${HOME}/nanodlp
+
+cp -r ${SOURCE_DIR}/nanodlp_db/* ${HOME}/nanodlp/db
+
+cp ${SOURCE_DIR}/service/nanodlp.service /etc/systemd/system/
+
+# TODO: Ensure script is run with sudo, else this will fail
+systemctl daemon-reload
+systemctl enable nanodlp.service
+systemctl start nanodlp.service
+
+if [ ! -d "${HOME}/kiauh" ] ; then
+    git clone https://github.com/th33xitus/kiauh.git ${HOME}/kiauh
+else
+    git -C "${HOME}/kiauh" pull
+fi
+
+cp ${SOURCE_DIR}/klipper_repos.txt ${HOME}/kiauh/
