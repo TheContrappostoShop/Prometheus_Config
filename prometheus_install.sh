2449b46967683aae2f58b52313dd1e85868bf165
Ada Phillips <ragwafire99@gmail.com>
2022-10-31 22:45:03 -0400

    add logs, and klipper config ask

--- prometheus_install.sh
+++ prometheus_install.sh
@@ -10,9 +10,22 @@ INSTALL_DIR="$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)"
 mkdir -p ${INSTALL_DIR}/printer_data/config
 mkdir -p ${INSTALL_DIR}/nanodlp
 
-# TODO: Modify printer.cfg mcu location to match connected serial device
+echo "Copying configuration files..."
+if [ ! "$(ls -A ${INSTALL_DIR}/printer_data/config)" ]; then
+cp -r ${SOURCE_DIR}/klipper_config/* ${INSTALL_DIR}/printer_data/config
+else
+    echo "Klipper configuration files detected. Do you wish to overrite them?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) cp -r ${SOURCE_DIR}/klipper_config/* ${INSTALL_DIR}/printer_data/config; break;;
+            No ) break;;
+        esac
+    done
+fi
+
 cp -r ${SOURCE_DIR}/klipper_config/* ${INSTALL_DIR}/printer_data/config
 
+echo "Installing NanoDLP..."
 if [ ! -d "${INSTALL_DIR}/nanodlp" ] ; then
     wget https://www.nanodlp.com/download/nanodlp.linux.arm64.stable.tar.gz -O - | tar -xz -C ${INSTALL_DIR}/nanodlp
 else
@@ -25,13 +38,25 @@ else
     done
 fi
 
-cp -r ${SOURCE_DIR}/nanodlp_db/* ${INSTALL_DIR}/nanodlp/db
+echo "Configuring NanoDLP..."
+if [ ! "$(ls -A ${INSTALL_DIR}/nanodlp/db)" ]; then
+    cp -r ${SOURCE_DIR}/nanodlp_db/* ${INSTALL_DIR}/nanodlp/db
+else
+    echo "NanoDLP configuration files detected. Do you wish to overrite them?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) cp -r ${SOURCE_DIR}/nanodlp_db/* ${INSTALL_DIR}/nanodlp/db; break;;
+            No ) break;;
+        esac
+    done
+fi
 
+echo "Setting up NanoDLP service (may require SUDO)..."
 sudo cp ${SOURCE_DIR}/service/nanodlp.service /etc/systemd/system/
-
 sudo systemctl daemon-reload
 sudo systemctl enable nanodlp.service
 
+echo "Cloning Klipper Install and Update Helper..."
 if [ ! -d "${INSTALL_DIR}/kiauh" ] ; then
     git clone https://github.com/th33xitus/kiauh.git ${INSTALL_DIR}/kiauh
 else
