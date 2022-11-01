c2cea0a6a1e48a3a58a20ef838b174cd8cbeea24
Ada Phillips <ragwafire99@gmail.com>
2022-10-31 22:04:36 -0400

    Replace HOME with sudo sensitive user dir

--- prometheus_install.sh
+++ prometheus_install.sh
@@ -5,16 +5,17 @@
 set -e
 
 SOURCE_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
+INSTALL_DIR = "$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6)"
 
-mkdir -p ${HOME}/printer_data/config
-mkdir -p ${HOME}/nanodlp
+mkdir -p ${INSTALL_DIR}/printer_data/config
+mkdir -p ${INSTALL_DIR}/nanodlp
 
 # TODO: Modify printer.cfg mcu location to match connected serial device
-cp -r ${SOURCE_DIR}/klipper_config/* ${HOME}/printer_data/config
+cp -r ${SOURCE_DIR}/klipper_config/* ${INSTALL_DIR}/printer_data/config
 
-wget https://www.nanodlp.com/download/nanodlp.linux.arm64.stable.tar.gz -O - | tar -xz -C ${HOME}/nanodlp
+wget https://www.nanodlp.com/download/nanodlp.linux.arm64.stable.tar.gz -O - | tar -xz -C ${INSTALL_DIR}/nanodlp
 
-cp -r ${SOURCE_DIR}/nanodlp_db/* ${HOME}/nanodlp/db
+cp -r ${SOURCE_DIR}/nanodlp_db/* ${INSTALL_DIR}/nanodlp/db
 
 cp ${SOURCE_DIR}/service/nanodlp.service /etc/systemd/system/
 
@@ -22,14 +23,14 @@ cp ${SOURCE_DIR}/service/nanodlp.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable nanodlp.service
 
-if [ ! -d "${HOME}/kiauh" ] ; then
-    git clone https://github.com/th33xitus/kiauh.git ${HOME}/kiauh
+if [ ! -d "${INSTALL_DIR}/kiauh" ] ; then
+    git clone https://github.com/th33xitus/kiauh.git ${INSTALL_DIR}/kiauh
 else
-    git -C "${HOME}/kiauh" pull
+    git -C "${INSTALL_DIR}/kiauh" pull
 fi
 
-cp ${SOURCE_DIR}/klipper_repos.txt ${HOME}/kiauh/
+cp ${SOURCE_DIR}/klipper_repos.txt ${INSTALL_DIR}/kiauh/
 
-./${HOME}/kiauh/kiauh.sh
+${INSTALL_DIR}/kiauh/kiauh.sh
 
 systemctl start nanodlp.service
