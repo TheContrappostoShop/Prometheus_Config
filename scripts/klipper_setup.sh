b8671b3f787d9738da516c1dc3785185a976e325
Ada Phillips <ragwafire99@gmail.com>
2022-11-10 12:40:32 -0500

    remove outdated package req

--- scripts/klipper_setup.sh
+++ scripts/klipper_setup.sh
@@ -2,7 +2,7 @@
 
 INSTALL_DIR=$1
 
-sudo apt install virtualenv python-dev libffi-dev build-essential libncurses-dev libusb-dev avrdude gcc-avr binutils-avr avr-libc stm32flash libnewlib-arm-none-eabi libusb-1.0 pkg-config -y
+sudo apt install virtualenv python-dev libffi-dev build-essential libncurses-dev libusb-dev avrdude gcc-avr binutils-avr avr-libc stm32flash libnewlib-arm-none-eabi pkg-config -y
 
 # Create virtualenv if it doesn't already exist
 [ ! -d ${INSTALL_DIR}/klippy_env ] && virtualenv -p python3 ${INSTALL_DIR}/klippy_env
