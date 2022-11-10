29d48e5023aadbd232d734ac2bf5b75ef2bed381
Ada Phillips <ragwafire99@gmail.com>
2022-11-10 12:48:01 -0500

    Fix spacing, fix name of klippy_env

--- scripts/klipper_setup.sh
+++ scripts/klipper_setup.sh
@@ -5,7 +5,7 @@ INSTALL_DIR=$1
 sudo apt install virtualenv python-dev libffi-dev build-essential libncurses-dev libusb-dev avrdude gcc-avr binutils-avr avr-libc stm32flash libnewlib-arm-none-eabi pkg-config -y
 
 # Create virtualenv if it doesn't already exist
-[ ! -d ${INSTALL_DIR}/klippy_env ] && virtualenv -p python3 ${INSTALL_DIR}/klippy_env
+[ ! -d ${INSTALL_DIR}/klippy-env ] && virtualenv -p python3 ${INSTALL_DIR}/klippy-env
 
 # Install/update dependencies
-${INSTALL_DIR}/klippy_env/bin/pip install -r ${INSTALL_DIR}/klipper/scripts/klippy-requirements.txt
+${INSTALL_DIR}/klippy-env/bin/pip install -r ${INSTALL_DIR}/klipper/scripts/klippy-requirements.txt
